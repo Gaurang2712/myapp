5b69d847577c36998703e44442602080b35da6e1
Gaurang2712 <gaurangchaudhary2712@gmail.com>
2024-12-16 23:06:50 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import AddUser from './components/AddUser';
+import UsersList from './components/UserList';
+import './App.css';
+
+function App() {
+  const [refreshTrigger, setRefreshTrigger] = useState(0);
+
+  const handleUserAdded = () => {
+    setRefreshTrigger(prev => prev + 1);
+  };
+
+  return (
+    <div>
+      <AddUser onUserAdded={handleUserAdded} />
+      <UsersList key={refreshTrigger} />
+    </div>
+  );
+}
+
+export default App;
